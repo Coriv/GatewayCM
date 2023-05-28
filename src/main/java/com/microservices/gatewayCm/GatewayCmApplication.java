428e15aac34a09beb68808001927c5fa9994ea68
Sebastian Boron <sebastian.boron8@gmail.com>
2023-05-28 21:25:34 +0200

    Initial gateway config

---
+++ src/main/java/com/microservices/gatewayCm/GatewayCmApplication.java
@@ -0,0 +1,15 @@
+package com.microservices.gatewayCm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayCmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayCmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microservices/gatewayCm/GatewayCmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.gatewayCm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayCmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
